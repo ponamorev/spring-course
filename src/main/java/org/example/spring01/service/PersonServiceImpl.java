96158db0ed347393d9bc559b0fdcaf1c5f966c00
Vyacheslav Ponamorev <penzayk@mail.ru>
2023-08-12 17:35:12 +0300

    init bean via property

--- src/main/java/org/example/spring01/service/PersonServiceImpl.java
+++ src/main/java/org/example/spring01/service/PersonServiceImpl.java
@@ -1,12 +1,15 @@
 package org.example.spring01.service;
 
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.example.spring01.dao.PersonDao;
 import org.example.spring01.domain.Person;
 
-@RequiredArgsConstructor
+//@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class PersonServiceImpl implements PersonService {
-    private final PersonDao dao;
+    private PersonDao dao;
 
     @Override
     public Person getByName(String name) {
