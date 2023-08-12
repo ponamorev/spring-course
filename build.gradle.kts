8d7db7651bafa7f6597e82c42620fc2d90ce09e5
Vyacheslav Ponamorev <penzayk@mail.ru>
2023-08-12 16:32:09 +0300

    add spring context

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,12 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+var spring = "6.0.8"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework:spring-context:$spring")
 }
--- src/main/java/org/example/spring01/Main.java
+++ src/main/java/org/example/spring01/Main.java
@@ -1,10 +1,14 @@
 package org.example.spring01;
 
 import org.example.spring01.domain.Person;
+import org.example.spring01.service.PersonService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        Person ivan = null;
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        PersonService service = context.getBean(PersonService.class);
+        Person ivan = service.getByName("ivan");
         System.out.println("Person: name = " + ivan.getName() + "; age = " + ivan.getAge());
     }
 }
